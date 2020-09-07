2e9daa87ad5fdb7956c63cc64286fa44ae3b83c2
Greg Bastianelli <greg@gerrg.com>
2020-09-07 13:05:11 -0400

    capitalize the detials and seperate into modules

--- part2/data-for-countries/src/Countries.js
+++ part2/data-for-countries/src/Countries.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+
 const Countries = ({ countries }) => {
   return countries.length === 1 ? (
     <CountryDetails country={countries[0]} />
@@ -12,14 +14,32 @@ const Countries = ({ countries }) => {
 
 const CountryListItem = ({ name }) => <p>{name}</p>;
 
+const Header = ({ name }) => <h1>{name}</h1>;
+const Detail = ({ header, value }) => (
+  <p>
+    {capitalize(header)}: {value}
+  </p>
+);
+
 const CountryDetails = ({ country }) => {
   console.log(country);
   return (
     <div class="show">
-      <h1>{country.name}</h1>
-      <p>Capital: {country.capital}</p>
-      <p>Population: {country.population}</p>
+      <Header name={country.name} />
+      <Detail header="capital" value={country.capital} />
+      <Detail header="Population" value={country.population} />
       <h4>Languages</h4>
+      <ul>
+        {country.languages.map((language) => (
+          <li>{language.name}</li>
+        ))}
+      </ul>
+      <h6>National Flag</h6>
+      <img
+        width={200}
+        src={country.flag}
+        alt={`${country.name}'s national flag`}
+      />
     </div>
   );
 };
